42d6e72c3300f15209d065fc52c9cecdd5616b8b
balbes150 <balbes-150@yandex.ru>
2017-10-03 12:56:09 +0300

    add Nougat-Ubuntu

--- board/khadas/configs/kvim2.h
+++ board/khadas/configs/kvim2.h
@@ -44,8 +44,9 @@
 
 /* configs for CEC */
 #define CONFIG_CEC_OSD_NAME		"KVim2"
+#define CONFIG_CEC_WAKEUP
 
-#define CONFIG_INSTABOOT
+//#define CONFIG_INSTABOOT
 
 /* config for kbi */
 #define CONFIG_KHADAS_KBI 1
@@ -84,8 +85,7 @@
 /* args/envs */
 #define CONFIG_SYS_MAXARGS  64
 #define CONFIG_EXTRA_ENV_SETTINGS \
-        "firstboot=0\0"\
-        "upgrade_step=0\0"\
+	"boardname=kvim2\0" \
         "jtag=disable\0"\
         "loadaddr=1080000\0"\
         "outputmode=1080p60hz\0" \
@@ -101,172 +101,74 @@
         "fb_addr=0x3d800000\0" \
         "fb_width=1920\0" \
         "fb_height=1080\0" \
-        "usb_burning=update 1000\0" \
         "fdt_high=0x20000000\0"\
-        "try_auto_burn=update 700 750;\0"\
-        "sdcburncfg=aml_sdc_burn.ini\0"\
-        "sdc_burning=sdc_burn ${sdcburncfg}\0"\
-        "wipe_data=successful\0"\
-        "wipe_cache=successful\0"\
-        "EnableSelinux=permissive\0"\
-        "recovery_part=recovery\0"\
-        "recovery_offset=0\0"\
-        "active_slot=_a\0"\
-        "boot_part=boot\0"\
-        "initargs="\
-            "rootfstype=ramfs init=/init console=ttyS0,115200 no_console_suspend earlyprintk=aml-uart,0xc81004c0 ramoops.pstore_en=1 ramoops.record_size=0x8000 ramoops.console_size=0x4000 "\
+	"init_display=" \
+		"osd open;" \
+		"osd clear;" \
+		"imgread pic logo bootup ${loadaddr};" \
+		"bmp display ${bootup_offset}; bmp scale" \
+	"\0"\
+	"start_autoscript=" \
+		"if usb start; then run start_usb_autoscript; if mmcinfo; then run start_mmc_autoscript;fi;fi;" \
+		"\0" \
+	"start_mmc_autoscript=" \
+		"if fatload mmc 0 1020000 s905_autoscript; then autoscr 1020000; fi" \
+		"\0"\
+	"start_usb_autoscript=" \
+		"if fatload usb 0 1020000 s905_autoscript; then autoscr 1020000; fi" \
+		"\0" \
+	"bootdisk=ramdisk\0" \
+	"initargs=" \
+		"root=LABEL=ROOTFS rootflags=data=writeback rw logo=osd1,loaded,0x3d800000,1080p60hz vout=1080p60hz,enable hdmimode=1080p60hz console=ttyS0,115200n8 console=tty0 no_console_suspend consoleblank=0 fsck.repair=yes net.ifnames=0\0" \
+	"storeargs="\
+		"setenv bootargs ${initargs} jtag=${jtag} ddr_size=${ddr_size};"\
+		"run mac_init;\0"\
+	"mac_init="\
+		"kbi ethmac;"\
+		"setenv bootargs ${bootargs} mac=${eth_mac} androidboot.mac=${eth_mac};\0"\
+	"combine_key="\
+		"saradc open 0;"\
+		"if saradc get_in_range 0x0 0x1f; then "\
+			"echo Detect function key;"\
+			"if gpio input GPIOAO_2; then "\
+			"echo Detect combine keys;"\
+			"store init 3; fi;"\
+		"fi;"\
+		"\0"\
+	"upgrade_key="\
+            "if gpio input GPIOAO_2; then "\
+                "echo detect upgrade key; sleep 1;"\
+                "if gpio input GPIOAO_2; then update; fi;"\
+            "fi;"\
             "\0"\
-        "upgrade_check="\
-            "echo upgrade_step=${upgrade_step}; "\
-            "if itest ${upgrade_step} == 3; then "\
-                "run init_display; run storeargs; run update;"\
-            "else fi;"\
-            "\0"\
-    "storeargs="\
-            "setenv bootargs ${initargs} androidboot.selinux=${EnableSelinux} logo=${display_layer},loaded,${fb_addr},${outputmode} maxcpus=${maxcpus} vout=${outputmode},enable hdmimode=${hdmimode} cvbsmode=${cvbsmode} hdmitx=${cecconfig} ddr_size=${ddr_size} cvbsdrv=${cvbs_drv}  pq=${pq} androidboot.firstboot=${firstboot} jtag=${jtag}; "\
-	"setenv bootargs ${bootargs} androidboot.hardware=amlogic;"\
-            "run cmdline_keys;"\
-            "setenv bootargs ${bootargs} androidboot.slot_suffix=${active_slot};"\
-            "\0"\
-        "switch_bootmode="\
-            "get_rebootmode;"\
-            "if test ${reboot_mode} = factory_reset; then "\
-                    "run recovery_from_flash;"\
-            "else if test ${reboot_mode} = update; then "\
-                    "run update;"\
-            "else if test ${reboot_mode} = cold_boot; then "\
-                /*"run try_auto_burn; "*/\
-            "else if test ${reboot_mode} = fastboot; then "\
-                "fastboot;"\
-            "fi;fi;fi;fi;"\
-            "\0" \
+	"vim2_check="\
+		"saradc open 1;"\
+		"if saradc get_in_range 0x1a0 0x220; then "\
+			"echo Product checking: pass!;"\
+		"else if saradc get_in_range 0x0 0x1cf; then "\
+			"echo Product checking: fail!; sleep 5; reboot;"\
+		"fi;fi;"\
+		"\0"\
+	"wol_init="\
+		"kbi powerstate;"\
+		"kbi trigger wol r;"\
+		"setenv bootargs ${bootargs} wol_enable=${wol_enable};"\
+		"if test ${power_state} = 1; then "\
+			"kbi trigger wol w 1;"\
+			"gpio set GPIODV_2;"\
+		"fi;\0"\
         "storeboot="\
-            "if imgread kernel ${boot_part} ${loadaddr}; then bootm ${loadaddr}; fi;"\
-            "run update;"\
-            "\0"\
-        "factory_reset_poweroff_protect="\
-            "echo wipe_data=${wipe_data}; echo wipe_cache=${wipe_cache};"\
-            "if test ${wipe_data} = failed; then "\
-                "run init_display; run storeargs;"\
-                "if mmcinfo; then "\
-                    "run recovery_from_sdcard;"\
-                "fi;"\
-                "if usb start 0; then "\
-                    "run recovery_from_udisk;"\
-                "fi;"\
-                "run recovery_from_flash;"\
-            "fi; "\
-            "if test ${wipe_cache} = failed; then "\
-                "run init_display; run storeargs;"\
-                "if mmcinfo; then "\
-                    "run recovery_from_sdcard;"\
-                "fi;"\
-                "if usb start 0; then "\
-                    "run recovery_from_udisk;"\
-                "fi;"\
-                "run recovery_from_flash;"\
-            "fi; \0" \
-         "update="\
-            /*first usb burning, second sdc_burn, third ext-sd autoscr/recovery, last udisk autoscr/recovery*/\
-            "run usb_burning; "\
-            "run sdc_burning; "\
-            "if mmcinfo; then "\
-                "run recovery_from_sdcard;"\
-            "fi;"\
-            "if usb start 0; then "\
-                "run recovery_from_udisk;"\
-            "fi;"\
-            "run recovery_from_flash;"\
-            "\0"\
-        "recovery_from_sdcard="\
-            "setenv bootargs ${bootargs} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
-            "if fatload mmc 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi;"\
-            "if fatload mmc 0 ${loadaddr} recovery.img; then "\
-                    "if fatload mmc 0 ${dtb_mem_addr} dtb.img; then echo sd dtb.img loaded; fi;"\
-                    "wipeisb; "\
-                    "bootm ${loadaddr};fi;"\
-            "\0"\
-        "recovery_from_udisk="\
-            "setenv bootargs ${bootargs} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
-            "if fatload usb 0 ${loadaddr} aml_autoscript; then autoscr ${loadaddr}; fi;"\
-            "if fatload usb 0 ${loadaddr} recovery.img; then "\
-                "if fatload usb 0 ${dtb_mem_addr} dtb.img; then echo udisk dtb.img loaded; fi;"\
-                "wipeisb; "\
-                "bootm ${loadaddr};fi;"\
-            "\0"\
-        "recovery_from_flash="\
-            "setenv bootargs ${bootargs} aml_dt=${aml_dt} recovery_part={recovery_part} recovery_offset={recovery_offset};"\
-            "if itest ${upgrade_step} == 3; then "\
-                "if ext4load mmc 1:2 ${dtb_mem_addr} /recovery/dtb.img; then echo cache dtb.img loaded; fi;"\
-                "if ext4load mmc 1:2 ${loadaddr} /recovery/recovery.img; then echo cache recovery.img loaded; wipeisb; bootm ${loadaddr}; fi;"\
-            "else fi;"\
-            "if imgread kernel ${recovery_part} ${loadaddr} ${recovery_offset}; then wipeisb; bootm ${loadaddr}; fi;"\
-            "\0"\
-        "init_display="\
-            "osd open;osd clear;imgread pic logo bootup $loadaddr;bmp display $bootup_offset;bmp scale"\
-            "\0"\
-        "cmdline_keys="\
-            "if keyman init 0x1234; then "\
-                "if keyman read usid ${loadaddr} str; then "\
-                    "setenv bootargs ${bootargs} androidboot.serialno=${usid};"\
-                    "setenv serial ${usid};"\
-                "fi;"\
-                "kbi ethmac;"\
-                "setenv bootargs ${bootargs} mac=${eth_mac} androidboot.mac=${eth_mac};"\
-                "if keyman read deviceid ${loadaddr} str; then "\
-                    "setenv bootargs ${bootargs} androidboot.deviceid=${deviceid};"\
-                "fi;"\
-            "fi;"\
-            "\0"\
-        "bcb_cmd="\
-            "get_valid_slot;"\
-            "\0"\
-        "combine_key="\
-            "saradc open 0;"\
-            "if saradc get_in_range 0x0 0x1f; then "\
-            "echo Detect function key;"\
-            "if gpio input GPIOAO_2; then "\
-                "echo Detect combine keys;"\
-                "store init 3; fi;"\
-            "fi;"\
-            "\0"\
-        "upgrade_key="\
-            "if gpio input GPIOAO_2; then "\
-                "echo detect upgrade key; sleep 3;"\
-                "if gpio input GPIOAO_2; then run update; fi;"\
-            "fi;"\
-            "\0"\
-        "vim2_check="\
-            "saradc open 1;"\
-            "if saradc get_in_range 0x1a0 0x220; then "\
-                "echo Product checking: pass!;"\
-            "else if saradc get_in_range 0x0 0x1cf; then "\
-                "echo Product checking: fail!; sleep 5; reboot;"\
-            "fi;fi;"\
-            "\0"\
-         "wol_init="\
-            "kbi powerstate;"\
-            "kbi trigger wol r;"\
-            "setenv bootargs ${bootargs} wol_enable=${wol_enable};"\
-            "if test ${power_state} = 1; then "\
-            "kbi trigger wol w 1;"\
-            "gpio set GPIODV_2;"\
-            "fi;"\
+            "ext4load mmc 1:5 1080000 uImage; ext4load mmc 1:5 10000000 uInitrd; ext4load mmc 1:5 20000000 kvim2.dtb;bootm 1080000 10000000 20000000"\
             "\0"\
 
 #define CONFIG_PREBOOT  \
-            "run bcb_cmd; "\
-            "run factory_reset_poweroff_protect;"\
-            "run upgrade_check;"\
             "run init_display;"\
-            "run storeargs;"\
             "run combine_key;" \
-            "run upgrade_key;" \
-            "run vim2_check;" \
+            "run storeargs;"\
             "run wol_init;"\
-            "forceupdate;" \
-            "run switch_bootmode;"
-#define CONFIG_BOOTCOMMAND "run storeboot"
+	    "run upgrade_key;"
+
+#define CONFIG_BOOTCOMMAND "run start_autoscript; run storeboot"
 
 //#define CONFIG_ENV_IS_NOWHERE  1
 #define CONFIG_ENV_SIZE   (64*1024)
@@ -290,7 +192,7 @@
  *    CONFIG_DDR_TYPE_DDR3     : DDR3
  *    CONFIG_DDR_TYPE_DDR4     : DDR4
  *    CONFIG_DDR_TYPE_AUTO     : DDR3/DDR4 auto detect */
-#define CONFIG_DDR_TYPE					CONFIG_DDR_TYPE_DDR4
+#define CONFIG_DDR_TYPE					CONFIG_DDR_TYPE_AUTO
 /* DDR channel setting, please refer hardware design.
  *    CONFIG_DDR0_RANK0        : DDR0 rank0
  *    CONFIG_DDR0_RANK01       : DDR0 rank0+1
@@ -496,6 +398,7 @@
 #define CONFIG_FS_FAT 1
 #define CONFIG_FS_EXT4 1
 #define CONFIG_LZO 1
+#define CONFIG_CMD_EXT2 1
 
 /* Cache Definitions */
 //#define CONFIG_SYS_DCACHE_OFF
